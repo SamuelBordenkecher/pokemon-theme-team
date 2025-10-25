83959ca4287d52fc9ab1e50e20bda76403513044
Samuel Bordenkecher <stb4380@gmail.com>
2025-10-25 12:31:13 -0400

    Basic Working Poke Generator

--- poke_main.js
+++ poke_main.js
@@ -1,13 +1,13 @@
 console.log("Connected")
 
-const genbutn = document.getElementById("generate");
+const genbutton = document.getElementById("generator");
 const pokeList = []
-genbutn.addEventListener("click", (event) => {
-    event.preventDefault();
-    genRandInt()
+genbutton.addEventListener("click", (event) => {
+    genRandInt();
 })
 
 function genRandInt(){
+    console.log("Random INt")
     let answer = Math.floor(Math.random() * 100);
     console.log(answer)
     getRandomPoke(answer);
@@ -20,9 +20,10 @@ function getRandomPoke(number) {
     .then ((pokeData) => {
         console.log("GEttingPokeData")
         pokeList.push(pokeData.name)
-        const pokeType = pokeData.types.type
+        console.log(pokeData.name)
+        const pokeType = pokeData.types[0].type.name
         console.log(pokeType)
-        getPokebyType(pokeType)
+        getPokeByType(pokeType)
 
     })
 }
@@ -32,14 +33,17 @@ function getPokeByType(pokeType) {
     .then((resp) => resp.json())
     .then((pokeData) => {
         let i = 0
-        while (i < 4) {
-            let num = Math.floor(Math.random() * 10)
-            pokeList.push(pokeData.pokemon[num].name)
+        while (i < 5) {
+            const length = pokeData.pokemon.length
+            let num = Math.floor(Math.random() * length)
+            console.log(pokeData.pokemon[num].pokemon.name)
+            pokeList.push(pokeData.pokemon[num].pokemon.name)
+            i++
         }
     createPokeTeam(pokeList)
     })
 }
-function createTeamDiv(pokeList) {
+function createPokeTeam(pokeList) {
     console.log('CREATING Poke Team')
     for (const pokemon of pokeList) {
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -50,11 +54,33 @@ function createTeamDiv(pokeList) {
             console.log(`Name: ${name} sprite: ${spriteURL}`)
             createPokeCard(pokeData);
         })
+    }
 }
 
 
-        
+function createPokeCard(pokeData) {
+    console.log("Creating CARD")
+    const name = pokeData.name
+    const spriteURL = pokeData.sprites.front_default;
 
+  // create the elements we need for our card
+    let div = document.createElement("div")
+    let img = document.createElement("img")
+    let h5 = document.createElement("h5")
+
+  // add the data to the card
+    h5.innerText = name;
+    img.src = spriteURL;
+    div.className = 'pokemon-card'
+    div.style.border = "solid blue 3px"
+
+  // add the h5 and img to the div
+      div.appendChild(h5)
+      div.appendChild(img)
+
+  // attach the div to my DOM
+    const container = document.getElementById('poke-container')
+    container.appendChild(div)
 }
 
 
